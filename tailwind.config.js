d9fb6b30a6f068fd26711bdc38da3ae3a02451f1
lucaszcai <lucas.z.cai@gmail.com>
2021-06-19 22:49:52 -0500

    placeholder page
    basic placeholder website with typewriter effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fill: theme => ({
+        'red': theme('colors.red.500'),
+        'green': theme('colors.green.500'),
+        'blue': theme('colors.blue.500'),
+      }),
+      colors: {
+        greenBlue: '#0066cc',
+        orangeYellow: '#F4D35E'
+      }
+    },
   },
   variants: {
     extend: {},
