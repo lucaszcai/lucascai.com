993f4c65fa7173088293a741d14ce4a03031a4ba
lucaszcai <lucas.z.cai@gmail.com>
2021-06-19 21:28:53 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,10 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          Hi I'm {" "}
+          <span className="text-blue-600">
+             Lucas
+          </span>
         </h1>
 
         <p className="mt-3 text-2xl">
