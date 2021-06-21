5b9056e6152d20363da4eafe391cba8059df9110
lucaszcai <lucas.z.cai@gmail.com>
2021-06-20 22:26:16 -0500

    Update useDarkMode.js

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -4,8 +4,6 @@ export default function useDarkMode(){
     const [theme, setTheme] = useState('dark');
     const colorTheme = theme === 'dark' ? 'light' : 'dark';
 
-    console.log(colorTheme);
-
     useEffect(() => {
         const root = window.document.documentElement;
    
