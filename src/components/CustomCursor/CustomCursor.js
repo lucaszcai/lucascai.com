a9ff5e28ba2c5204e5947e672271bf169e0504ac
lucaszcai <lucas.z.cai@gmail.com>
2022-12-19 16:01:05 -0600

    test

--- src/components/CustomCursor/CustomCursor.js
+++ src/components/CustomCursor/CustomCursor.js
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 
 export const CustomCursor = () => {
     const cursorRef = React.useRef(null);
+    let enlarge = false;
 
     React.useEffect(()=> {
         document.addEventListener("mousemove", (event) => {
@@ -9,8 +10,24 @@ export const CustomCursor = () => {
             const mouseX = clientX - cursorRef.current.clientWidth / 2;
             const mouseY = clientY - cursorRef.current.clientHeight / 2;
             cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px,0)`;
+           
         });
     }, []);
+
+    React.useEffect(() => {
+        document.addEventListener("mouseover", (event) => {
+            cursorRef.current.style.width = `80px`;
+            cursorRef.current.style.height = `80px`;
+        })
+    })
+
+    React.useEffect(() => {
+        document.addEventListener("mouseout", (event) => {
+            cursorRef.current.style.width = `50px`;
+            cursorRef.current.style.height = `50px`;
+            console.log("mouse out");
+        })
+    })
 
     return <div className="app-cursor" ref={cursorRef}></div>
 }
